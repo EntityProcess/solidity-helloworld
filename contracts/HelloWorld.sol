84c4fa13ad7311e9bf09e2142249e89569919fdb
Christopher Tso <christso@gmail.com>
2017-10-26 09:42:59 +1100

    first commit

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.4;
+
+contract HelloWorld {
+    address public owner;
+    mapping (address => uint) balances;
+
+    function HelloWorld() public {
+        owner = msg.sender;
+        balances[owner] = 1000;
+    }
+    
+    function transfer(address _to, uint _value) public returns (bool success) {
+        if (balances[msg.sender] < _value) {
+            return false;
+        }
+
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        return true;
+    }
+
+    function getBalance(address _user) public constant returns (uint _balance) {
+        return balances[_user];
+    }
+}
